17b71e6aeefc3a13b6c5c69f0d4049cbe885bb91
Devillzify <andresmunoz@paucasesnovescifp.cat>
2022-12-18 16:10:52 +0100

    subida info extra

---
+++ fluttergetx/lib/info_extra.dart
@@ -0,0 +1,28 @@
+/* 
+
+======================================
+Leer informacion entre paginas:
+
+instalar dependencias usando "Get"
+
+crear clase proveedora extends GetxController
+
+instanciarlo con Get.put(nombreClaseController)
+
+Controller controller = Get.put(Controller()); crear instancia como en Java y ya
+
+ya podemos acceder a las variables y funciones
+
+
+=======================================
+Cambiar entre paginas:
+
+Get.to(nombreClasepagina)
+Get.back(): volver
+
+Si tenemos rutas nombradas usamos Get.toNamed(nombre)
+
+
+
+
+*/
